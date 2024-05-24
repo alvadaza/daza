295af7ce4b6ac2f3b31ccdce147c0a1055923e50
github_username <am.company.edu@gmail.com>
2024-05-24 15:29:34 -0500

    implementacion scrypt menu

--- script.js
+++ script.js
@@ -51,3 +51,28 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+/*activar el muenu de por que elegirno */
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(event, tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemeu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemeu.style.right = "0";
+}
+
+function closemenu() {
+  sidemeu.style.right = "-200px";
+}
