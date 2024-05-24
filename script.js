5543b640195dc0567fe8134a1f68265ecf22aeaf
github_username <am.company.edu@gmail.com>
2024-05-23 22:52:50 -0500

    implementacion stilos

--- script.js
+++ script.js
@@ -38,3 +38,16 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
       alert("Hubo un error al enviar el mensaje");
     });
 });
+
+var prevScrollpos = window.pageYOffset;
+var navbar = document.getElementById("navbar");
+
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    navbar.style.top = "0";
+  } else {
+    navbar.style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+};
